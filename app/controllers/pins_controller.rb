463ddf860f3454cb0e5cad7cae3ab677e5d20df1
Naina Raisinghani <nainar@chromium.org>
2016-02-06 23:21:11 +1100

    add user and pins and add relevant association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -10,13 +10,11 @@ class PinsController < ApplicationController
 	end
 
 	def new
-		@pin = Pin.new
-		# @pin = current_user.pins.build
+		@pin = current_user.pins.build
 	end
 
 	def create
-		@pin = Pin.new(pin_params)
-		# @pin = current_user.pins.build(pin_params)
+		@pin = current_user.pins.build(pin_params)
 
 		if @pin.save
 			redirect_to @pin, notice: "Pin was successfully created"
