aa27bc213b739d7388d6d2127a2df604b04627fc
Naina Raisinghani <nainar@chromium.org>
2016-02-06 23:46:50 +1100

    add image uploading with paperclip

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Pinterest
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.initialize_on_precompile = false
+    Paperclip.options[:command_path] = "/usr/local/bin/identify"
   end
 end
